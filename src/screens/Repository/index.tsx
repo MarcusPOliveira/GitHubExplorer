07a46491c28b89ce46cc73831e8c8ac0ee4f66cf
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2022-04-20 10:46:19 -0300

    🎉 aplicação completa 🎉

--- src/screens/Repository/index.tsx
+++ src/screens/Repository/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useRoute } from '@react-navigation/core';
 import { Linking } from 'react-native';
-import { useRepositories } from '../../hooks/useRepositories';
 
+import { useRepositories } from '../../hooks/useRepositories';
+import { TitleAnimation } from './TitleAnimation';
 import { Background } from '../../components/Background';
 import { Card } from '../../components/Card';
 
@@ -24,7 +25,6 @@ import {
   OpenIssuesText,
   IssuesList,
 } from './styles';
-import { TitleAnimation } from './TitleAnimation';
 
 interface RepositoryParams {
   repositoryId: number;
@@ -37,47 +37,55 @@ export function Repository() {
   const repository = findRepositoryById(repositoryId);
 
   function handleIssueNavigation(issueUrl: string) {
-    // TODO - use Linking to open issueUrl in a browser
+    Linking.openURL(issueUrl)
   }
 
   return (
     <Background>
       <Container>
         <RepoInfo>
-          {/* <OwnerAvatar source={{ uri:  }} /> */}
+          {<OwnerAvatar source={{ uri: repository.owner.avatar_url }} />}
 
           <TextGroup>
             <TitleAnimation>
               {
-                // TODO - full name of the repository
+                repository.full_name
               }
             </TitleAnimation>
 
-            <Description numberOfLines={2}>{
-              //TODO - repository description
-            }</Description>
+            <Description numberOfLines={2}>
+              {
+                repository.description
+              }
+            </Description>
           </TextGroup>
         </RepoInfo>
 
         <RepoStats>
           <Stars>
-            <StarsCounter>{
-              // TODO - repository stargazers count
-            }</StarsCounter>
+            <StarsCounter>
+              {
+                repository.stargazers_count
+              }
+            </StarsCounter>
             <StarsText>Stars</StarsText>
           </Stars>
 
           <Forks>
-            <ForksCounter>{
-              // TODO - repository forks count
-            }</ForksCounter>
+            <ForksCounter>
+              {
+                repository.forks_count
+              }
+            </ForksCounter>
             <ForksText>Forks</ForksText>
           </Forks>
 
           <OpenIssues>
-            <OpenIssuesCounter>{
-              // TODO - repository issues count
-            }</OpenIssuesCounter>
+            <OpenIssuesCounter>
+              {
+                repository.open_issues_count
+              }
+            </OpenIssuesCounter>
             <OpenIssuesText>Issues{'\n'}Abertas</OpenIssuesText>
           </OpenIssues>
         </RepoStats>
@@ -88,12 +96,12 @@ export function Repository() {
           showsVerticalScrollIndicator={false}
           renderItem={({ item: issue }) => (
             <Card
+              onPress={() => handleIssueNavigation(issue.html_url)}
               data={{
                 id: issue.id,
                 title: issue.title,
                 subTitle: issue.user.login,
               }}
-            // TODO - onPress prop calling 
             />
           )}
         />
